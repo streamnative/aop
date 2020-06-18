a4101d55cc3e7e0e9f0c4a2f22bb802fa9d80203
zhanghao <zhanghao1@cmss.chinamobile.com>
2020-06-18 20:37:19 +0800

    bug fix.

--- amqp-impl/src/test/java/io/streamnative/pulsar/handlers/amqp/test/frame/AmqpClientChannel.java
+++ amqp-impl/src/test/java/io/streamnative/pulsar/handlers/amqp/test/frame/AmqpClientChannel.java
@@ -36,7 +36,7 @@ public class AmqpClientChannel {
         Object object = responses.poll();
         while (object == null && count > 0) {
             try {
-                Thread.sleep(10);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }

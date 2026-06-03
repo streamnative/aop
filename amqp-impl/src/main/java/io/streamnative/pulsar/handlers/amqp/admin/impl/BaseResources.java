84afa39cc150e24a81fb301c2a97c4ae4ca5b59e
gaoran_10@126.com <gaoran_10@126.com>
2026-06-03 12:15:54 +0800

    fix

--- amqp-impl/src/main/java/io/streamnative/pulsar/handlers/amqp/admin/impl/BaseResources.java
+++ amqp-impl/src/main/java/io/streamnative/pulsar/handlers/amqp/admin/impl/BaseResources.java
@@ -20,7 +20,6 @@ import io.streamnative.pulsar.handlers.amqp.QueueContainer;
 import io.streamnative.pulsar.handlers.amqp.QueueService;
 import io.streamnative.pulsar.handlers.amqp.admin.model.VhostBean;
 import java.net.URI;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
